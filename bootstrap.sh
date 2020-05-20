4e1b965a967b9620ca951113e0d21df9942abebd
satsuper <satsuper@users.noreply.github.com>
2020-05-20 19:04:25 +1000

    Update bootstrap script with more logging.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-echo "\nBootstrap:\nconfigpath=$CONFIGPATH\nworldpath=$WORLDPATH\nlogpath=$LOGPATH\n"
+echo -e "\nBootstrap:\nconfigpath=$CONFIGPATH\nworldpath=$WORLDPATH\nlogpath=$LOGPATH\n"
 echo "Copying plugins..."
+ls
 cp -Rfv /plugins/* ./ServerPlugins
 
 mono --server --gc=sgen -O=all TerrariaServer.exe -configPath "$CONFIGPATH" -worldpath "$WORLDPATH" -logpath "$LOGPATH" "$@" 
